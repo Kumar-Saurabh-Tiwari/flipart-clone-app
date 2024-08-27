5ac828240df44ff7b783dfc293954a941e950789
Kumar-Saurabh-Tiwari <tiwarisaurabhkumar0@gmail.com>
2024-08-27 11:26:27 +0530

    slider added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,13 +8,17 @@ import { CartService } from 'src/app/service/cart.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
+  private currentIndex: number = 0;
+  private intervalId: any;
   public productList : any ;
   public filterCategory : any
   searchKey:string ="";
+  currentSlide = 0;
+  slideInterval: any;
   constructor(private api : ApiService, private cartService : CartService) { }
 
   ngOnInit(): void {
+    this.startAutoSlide();
     this.api.getProduct()
     .subscribe(res=>{
       this.productList = res;
@@ -44,4 +48,40 @@ export class ProductsComponent implements OnInit {
     })
   }
 
+  startAutoSlide() {
+    const slider = document.getElementById('slider');
+    this.intervalId = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % 4; // Assuming 4 images
+      slider!.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+    }, 4000); // 4 seconds
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+
+  nextSlide(): void {
+    const slider = document.getElementById('slider') as HTMLElement;
+    this.currentSlide = (this.currentSlide + 1) % slider.children.length;
+    this.updateSlider();
+  }
+
+  prevSlide(): void {
+    const slider = document.getElementById('slider') as HTMLElement;
+    this.currentSlide = (this.currentSlide - 1 + slider.children.length) % slider.children.length;
+    this.updateSlider();
+  }
+
+  updateSlider(): void {
+    const slider = document.getElementById('slider') as HTMLElement;
+    const slideWidth = slider.clientWidth;
+    slider.style.transform = `translateX(-${this.currentSlide * slideWidth}px)`;
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
 }
